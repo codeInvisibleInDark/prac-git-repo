d9ce887c3685dcc8ec8c58729774b68bb8f3cf00
codeInvisibleInDark <bscs2312181@szabist.pk>
2025-12-05 19:47:50 +0500

    second commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("git repository created");
 Console.WriteLine("git first commit");
+Console.WriteLine("git second commit");
